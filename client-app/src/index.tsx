c3d69347bbe2e824dc5efbe533f80d75c15b01e0
Olajide Osho <olajide.osho@wemabank.com>
2020-09-21 04:24:11 +0100

    End of section 10

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./app/layout/styles.css";
-import { BrowserRouter } from "react-router-dom";
+import { Router } from "react-router-dom";
+import {createBrowserHistory} from 'history';
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
 import ScrollToTop from "./app/layout/ScrollToTop";
+import 'react-toastify/dist/ReactToastify.min.css';
+
+export const history = createBrowserHistory()
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router history={history}>
     <ScrollToTop />
     <App />
-  </BrowserRouter>,
+  </Router>,
   document.getElementById("root")
 );
 
